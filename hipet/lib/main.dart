3927aef385dd52fa9f190d52a5995bd239e51b44
sirasatarato <sirasatarato@gmail.com>
2021-04-24 22:36:49 +0900

    Change MaterialApp to GetMaterialApp

--- hipet/lib/main.dart
+++ hipet/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hipet/src/configs/themes.dart';
+import 'package:hipet/src/pages/sign_page.dart';
 import 'package:hipet/src/pages/splash_page.dart';
 
 void main() {
@@ -10,21 +11,14 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Future.delayed(Duration(seconds: 3)),
-      builder: (context, AsyncSnapshot snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: SplashPage(),
-          );
-        } else {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: Scaffold(body: Center(child: Text('App loaded'))),
-          );
-        }
-      },
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: getThemeData(),
+      home: FutureBuilder(
+        future: Future.delayed(Duration(seconds: 0)),
+        builder: (context, AsyncSnapshot snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting ? SplashPage() : SignPage(),
+      ),
     );
   }
 }
