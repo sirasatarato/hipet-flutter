26fb502b7073d2c210e145d99cbd77458d667255
sirasatarato <sirasatarato@gmail.com>
2021-04-28 11:28:47 +0900

    Add ClickEvent

--- hipet/lib/src/widgets/widest_button.dart
+++ hipet/lib/src/widgets/widest_button.dart
@@ -4,41 +4,45 @@ class WidestButton extends StatelessWidget {
   final String text;
   final String? imageAsset;
   final bool isColored;
+  final VoidCallback? clickEvent;
 
-  const WidestButton(this.text, {this.imageAsset, this.isColored = false});
+  const WidestButton(this.text, {this.imageAsset, this.isColored = false, this.clickEvent});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(8),
-        color: isColored ? Theme.of(context).accentColor : Colors.white,
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          imageAsset != null
-              ? Image.asset(
-            imageAsset!,
-            fit: BoxFit.cover,
-            width: 16,
-            height: 16,
-          ) : SizedBox(width: 16),
-          Expanded(
-            child: Center(
-              child: Text(
-                  text,
-                  style: Theme.of(context).textTheme.button!.copyWith(
-                    color: isColored ? Colors.white : Colors.black,
-                  ),
+    return GestureDetector(
+      onTap: clickEvent,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(8),
+          color: isColored ? Theme.of(context).accentColor : Colors.white,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            imageAsset != null
+                ? Image.asset(
+              imageAsset!,
+              fit: BoxFit.cover,
+              width: 16,
+              height: 16,
+            ) : SizedBox(width: 16),
+            Expanded(
+              child: Center(
+                child: Text(
+                    text,
+                    style: Theme.of(context).textTheme.button!.copyWith(
+                      color: isColored ? Colors.white : Colors.black,
+                    ),
+                ),
               ),
             ),
-          ),
-          SizedBox(width: 16),
-        ],
+            SizedBox(width: 16),
+          ],
+        ),
       ),
     );
   }
